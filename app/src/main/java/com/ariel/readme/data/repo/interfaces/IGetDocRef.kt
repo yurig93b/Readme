1973ae58c4e3f5524d07a133903e59793d2ad1c5
Grigorian <yuri.grigorian@broadcom.com>
2021-11-23 01:42:42 +0200

    update push token

--- app/src/main/java/com/ariel/readme/data/repo/interfaces/IGetDocRef.kt
+++
@@ -1,7 +0,0 @@
-package com.ariel.readme.data.repo.interfaces
-
-import com.google.firebase.firestore.DocumentReference
-
-interface IGetDocRef: IGetFailure {
-    fun onSuccess(d:DocumentReference?)
-}
