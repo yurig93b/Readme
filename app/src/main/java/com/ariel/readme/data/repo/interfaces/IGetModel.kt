dd98003b72e59f00fae7923bae7feaa8792a30f2
yurig93b <46708202+yurig93b@users.noreply.github.com>
2021-11-23 01:43:11 +0200

    Push service (#5)
    * update push token
    
    * update push token
    
    * update push token

--- app/src/main/java/com/ariel/readme/data/repo/interfaces/IGetModel.kt
+++
@@ -1,8 +0,0 @@
-package com.ariel.readme.data.repo.interfaces
-
-import com.google.firebase.firestore.DocumentReference
-import com.google.firebase.firestore.DocumentSnapshot
-
-interface IGetModel<Model> : IGetFailure {
-    fun onSuccess(d: Model?, raw: DocumentSnapshot?, docRef: DocumentReference?)
-}
