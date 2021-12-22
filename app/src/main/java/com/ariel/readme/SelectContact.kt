7233a3b308f99978991d71eb0008fc4a8e6cce82
rawan <rawanash18@gmail.com>
2021-12-22 16:21:38 +0200

    add Contact at phone

--- app/src/main/java/com/ariel/readme/SelectContact.kt
+++ app/src/main/java/com/ariel/readme/SelectContact.kt
@@ -25,13 +25,13 @@ class SelectContact : AppCompatActivity() {
     private val Contact_Pick = 2
 
     @RequiresApi(Build.VERSION_CODES.N)
-    override fun onCreate(savedInstanceState: Bundle?) {//מכאן לוחצים כדי לבחור איש קשר
+    override fun onCreate(savedInstanceState: Bundle?) {//From here click to select a contact
         super.onCreate(savedInstanceState)
         binding = ActivitySelectContactBinding.inflate(layoutInflater)
         setContentView(binding.root)
         viewModel = ViewModelProvider(this).get(SelectContactViewModel::class.java)
         viewModel.checktest()
-        //בודק אם הוקש על הכפתור
+        //Checks if the button was pressed
         binding.addCon.setOnClickListener {
             if (check()) {
                 //allow
@@ -43,17 +43,17 @@ class SelectContact : AppCompatActivity() {
 
         }
     }
-    private fun pickContact() {//בוחר איש קשר
+    private fun pickContact() {//Selects a contact
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE
         startActivityForResult(intent, Contact_Pick)
     }
-    private fun requestContact() {//מבקש אישור ליצירת איש קשר
+    private fun requestContact() {//Requests permission to contact
         val permission = arrayOf(android.Manifest.permission.READ_CONTACTS)
         ActivityCompat.requestPermissions(this, permission, Contact_Permission)
 
     }
-    private fun check(): Boolean {//בודק הרשאת יצירת קשר
+    private fun check(): Boolean {//Checking contact permission
         return ContextCompat.checkSelfPermission(
             this,
             android.Manifest.permission.READ_CONTACTS
@@ -82,14 +82,14 @@ class SelectContact : AppCompatActivity() {
         requestCode: Int,
         resultCode: Int,
         data: Intent?
-    ) {//מטפל אחרי שבוחרים איש קשר
+    ) {//Treats after choosing a contact
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK) {
             if (requestCode == Contact_Pick) {
                 binding.listContacts.text = ""
                 val cursor1: Cursor
                 val cursor2: Cursor?
-                val uri: Uri? = data?.data//מקבלים נתונים מ intent
+                val uri: Uri? = data?.data//Get data from intent
                 cursor1 = contentResolver.query(uri!!, null, null, null, null)!!
                 if (cursor1.moveToFirst()) {//מקבלים פרטים ליצירת איש קשר
                     val contactId =
@@ -109,25 +109,25 @@ class SelectContact : AppCompatActivity() {
                     viewModel.ensureChat(contactNumber)
 
                     //binding.listContacts.append("\nId:$contactId")
-                    //מה רושמים בטקסט מגדירים את הפרטים
+                    //What is written in the text defines the details
 //                    binding.listContacts.append("\nName:$contactName")
 //                    binding.listContacts.append("\nPhone: $contactNumber")
                     // binding.listContacts.append("\nImage:$contactPhoto")
                     //מגדיר תמונה
 //                        if(contactPhoto!=null){
-//                            binding.photoo.setImageURI(Uri.parse(contactPhoto))      //אם יש תמונה מגדירים אותה
+//                            binding.photoo.setImageURI(Uri.parse(contactPhoto))      //If there is a picture define it
 //                        }
 //                        else{
-//                            binding.photoo.setImageResource(R.drawable.userpro)//אם אין תמונה בוחרים תמונה דיפולתיבית
+//                            binding.photoo.setImageResource(R.drawable.userpro)//If there is no image choose a dipulatory image
 //
 //                        }
-//                        //בודקים אם לאיש קשר יש מספר טלפון
+// // Check if the contact has a phone number
 //                        if(idResultHold==1){
 //                            cursor2=contentResolver.query(
 //                                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,
 //                                ContactsContract.CommonDataKinds.Phone.CONTACT_ID+" "+contactId,null,
 //                                null)
-//                        //אם לאותו איש קשר יש יותר ממספר אחד
+//                        //If the same contact has more than one number
 //                            while(cursor2!!.moveToNext()){
 //                                //get phone num
 //                                val contactNum=cursor2.getString(cursor2.getColumnIndex(
@@ -143,7 +143,7 @@ class SelectContact : AppCompatActivity() {
 
                 }
             } else {
-                //ביטול בחירת איש קשר
+                //Cancel contact selection
                 Toast.makeText(this, "Canceled", Toast.LENGTH_SHORT).show()
 
             }
