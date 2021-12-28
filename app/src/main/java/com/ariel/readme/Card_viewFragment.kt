cdd41de3473ed2fb2162f9ba28feff163e4d58eb
rawan <rawanash18@gmail.com>
2021-12-28 23:08:48 +0200

    adapter and fragment card

---
+++ app/src/main/java/com/ariel/readme/Card_viewFragment.kt
@@ -0,0 +1,30 @@
+package com.ariel.readme
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Card_viewFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_card_view, container, false)
+    }
+
+    companion object {
+
+        fun newInstance()=Card_viewFragment
+//                    (param1: String, param2: String) =
+//            card_view2().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+    }
+}
