95fc46facdce6a35e8fb5417784a398b2499ea3c
rawan <rawanash18@gmail.com>
2021-12-29 20:08:45 +0200

    chatrepo

--- app/src/main/java/com/ariel/readme/data/repo/ChatRepository.kt
+++ app/src/main/java/com/ariel/readme/data/repo/ChatRepository.kt
@@ -3,6 +3,7 @@ package com.ariel.readme.data.repo
 import android.os.Build
 import androidx.annotation.RequiresApi
 import com.ariel.readme.data.model.Chat
+import com.ariel.readme.data.model.HotWord
 import com.ariel.readme.data.model.User
 import com.ariel.readme.data.repo.interfaces.IChatRepository
 import com.google.android.gms.tasks.Task
@@ -34,12 +35,16 @@ class ChatRepository : FirebaseRepository<Chat>(), IChatRepository {
         val participantsStr = participants.sorted().stream().collect(Collectors.joining(","))
         return HookQuery(collRef.whereEqualTo(Chat::participantsStr.name, participantsStr).get())
     }
-    override fun listenOnChats(user: User, listener: EventListener<QuerySnapshot>): ListenerRegistration {
-        return collRef.whereArrayContains(Chat::participants.name, user.uid!!).addSnapshotListener(listener)
-    }
-//    fun listenOnChats(uid: String, listener: IGetChangedModels<Chat>): ListenerRegistration {
-//        return HookListenQuery(collRef.whereArrayContains(Chat::participants.name, uid!!),listener)
+//    override fun listenOnChats(user: User, listener: EventListener<QuerySnapshot>): ListenerRegistration {
+//        return collRef.whereArrayContains(Chat::participants.name, user.uid!!).addSnapshotListener(listener)
 //    }
+    override fun listenOnChats(uid: String, listener: IGetChangedModels<Chat>): ListenerRegistration {
+        return HookListenQuery(collRef.whereArrayContains(Chat::participants.name, uid!!),listener)
+    }
 
-}
+    fun removeChat( cid: String): Task<Void> {
+        return collRef.document(cid).delete()
+    }
+            }
+
 
